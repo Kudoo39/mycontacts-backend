4ffe82d546e311820c78f3a837aad82b6f7cf926
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2023-08-16 01:16:55 +0300

    CRUD create new contact & get all contacts

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,7 +1,9 @@
 const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
 
 const getContacts = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get all contacts" });
+  const contacts = await Contact.find();
+  res.status(200).json({ contacts });
 });
 
 const postContact = asyncHandler(async (req, res) => {
@@ -11,7 +13,14 @@ const postContact = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("All fields are mandatory!");
   }
-  res.status(201).json({ message: "Create contacts" });
+
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+  });
+
+  res.status(201).json({ contact });
 });
 
 const getContact = asyncHandler(async (req, res) => {
