638323c748bdcdce66d5652f5f7b8059ea19e988
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2023-08-16 05:41:31 +0300

    user register & password hash

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,40 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+
 //@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
+
+  //if there is user already =>
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User already registered!");
+  }
+
+  //if no user => create new
+  //Hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log("Hashed Password", hashedPassword);
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  console.log("User created " + user);
+  if (user) {
+    res.status(201).json({ _id: user.id, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("User data not valid");
+  }
   res.json({ message: "Register the user" });
 });
 
